1142e19d2719f564284709424ffc0eb0297f41b9
Ocheltree <socheltree@integrity-apps.com>
2018-04-16 15:17:34 -0700

    End of Tutorial

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/post_index';
 import PostsNew from './components/post_new';
+import PostsShow from './components/post_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -17,6 +18,7 @@ ReactDOM.render(
       <div>
         <Switch>
           <Route path="/posts/new" component={PostsNew} />
+          <Route path="/posts/:id" component={PostsShow} />
           <Route path="/" component={PostsIndex} />
         </Switch>
       </div>
